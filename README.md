7a2c971cd2ae7ae69ed4f3ca87f4a8f6e243938b
Hoyoung <hoyoung@cn>
2022-02-17 01:07:17 +0800

    Init

---
+++ README.md
@@ -0,0 +1,39 @@
+## 蓝奏云直链获取工具
+
+###### lanzou direct link Helper
+
+##### 一开始是想找个解析工具放在Android app里用来更新的，但发现Java版本的最短的都一年没有更新了，还是自己摸个简单的能用就好。
+
+- 依赖 okhttp3，jsoup
+- 支持新旧两种蓝奏云页面，文件名、大小和直链
+
+##### Example：
+
+```java
+import LanzouHelper;
+
+class Test {
+    
+    public static void main(String[] args) {
+        ArrayList<String> urls = new ArrayList<>();
+        //企业链接
+        urls.add("https://macwk.lanzouo.com/iRH0wwq0m8d");
+        //个人链接
+        urls.add("https://vincentapps.lanzouo.com/i4uS3lmp56j");
+
+        for (String url : urls) {
+            System.out.println(LanzouHelper.getLanZouRealLink(url));
+        }
+
+        /**
+         name='Karabiner-Elements_14.3.0_(最低11.0)__macwk.com.dmg'
+         size='9.8 M'
+         dlLink='https://developer.lanzoug.com/file/?UjQHOQw9Dj8DClFpVmMHawA/UGgHEwRnAXRWMVNtUjpXOQIwCSZTKlNAATsFNFM/U2xVPlN4VCVWDlFmUztSfVJhB34MYQ4FA31R4VaeB4YAvlDoB9YENwE3Vn5TP1J6VwgCCgk5U2ZTZgEgBTpTfFNqVT9TYVR4VjVROlNoUnVSNAc5DD0OPwMKUW5WZgc7AG9QYwduBD4BP1ZhUzhSYFdxAjMJJ1M6UzcBZwVmU2RTMVVmUzRUYFZ3USdTf1JuUmAHYAxjDmsDelE2VjMHKQBoUGQHdwQ0ATZWaVNsUjVXMwJjCTZTP1M1AW8FY1NlUztVYFM4VDJWNFFkUzhSYFIzB2QMNA47A21RNFZmBzYAPlBlBz4EKAFiVj1TaFJ1VyICJgkxU3VTbAEzBWxTalMwVWZTPFRlVmVRcVN7UjpSPwc1DDcOZwNkUTFWNgczAGpQZgdpBDYBM1ZoUyFSfVdxAjMJOFNwUzgBZgVnU2ZTPFVgUz5Ub1ZlUWJTPVJ1UicHIAwmDmcDZFExVjEHMQBuUGAHawQzATdWaVMpUiZXPgIlCWlTNlM0AWEFf1NgUz5VflM9VGJWZ1F5Uz5SZVJg'
+
+         name='JDCookie.apk'
+         size='2.6 M'
+         dlLink='https://developer.lanzoug.com/file/?B2FVawg5BjdVXAI6BDECbgc4DzdVQAdBA0cAaVJhUDpSO1JgXS4FMAQiATwHdVQzVmVSO1JoUw4EagBiXD1UZgc/VTIIYAZqVTYCYQRpAiUHOw95VTcHNwMzADNSOVBpUmBSNV0mBSEEIgFqB2FUZVY+UmZSIlNhBDEAKVwxVGYHKFU1CGQGMVViAjcEMgJiB20POVUyBzUDMwAxUmxQYFJkUjJdMAVjBGoBNQdlVDBWO1JkUmtTZAQ1AGVcZFQwB2ZVLAg0BiJVaAJyBCUCcAc4D3hVYwdhAzkAMFI6UGJSZlIwXTcFdwQmAT4HPlQwVmpSalI8U2cENwAzXDBUZgc2VTIIYAZrVS0CegR2AmUHMQ99VTcHNAMyADJSO1BhUmBSN103BWIEYwFxByZUJVZ7UmpSPFNnBDIANVwyVGUHNVU3CGwGYFUlAiEEOQJzB2APO1U7BzMDKgA0UjlQf1JjUjFdNgV/BGMBYQdh'
+         */
+    }
+}
+```
---
+++ src/main/java/LanzouHelper.java
@@ -0,0 +1,159 @@
+import okhttp3.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LanzouHelper {
+    // 公用标准请求头
+    static Headers.Builder builder = new Headers.Builder();
+    static {
+        builder.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+        builder.add("Accept-Encoding", "gzip, deflate");
+        builder.add("Upgrade-Insecure-Requests", "1");
+        builder.add("accept-language", "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6,ja;q=0.5");
+        builder.add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
+    }
+
+
+    public static Lanzou getLanZouRealLink(String url) {
+        String mainHost = getFullHost(url);
+
+        // 添加蓝奏云特有的header
+        Headers headers = builder.set("referer", url).build();
+
+        String name = null, size = null;
+        try {
+            // 加载页面
+            Document doc = Jsoup.connect(url).get();
+
+
+            // 获取文件名和大小
+            Elements title1 = doc.select("div[style=font-size: 30px;text-align: center;padding: 56px 0px 20px 0px;]");
+            if (title1.size() != 0) {
+                name = title1.get(0).html();
+            } else {
+                name = doc.select("#filenajax").html();
+            }
+
+            Elements size1 = doc.select("meta[name=description]");
+            if (size1.size() != 0) {
+                size = regex(size1.attr("content"), "\\d.\\d(?: )*[A-Za-z]+");
+            }
+
+            // 获取与拼接下载页（iframe）地址
+            Elements realPage = doc.select("iframe");
+            String realUrl = "https://" + mainHost + realPage.attr("src");
+            doc = Jsoup.connect(realUrl).get();
+
+            // 正则分析动态参数
+            String js = doc.select("script[type=text/javascript]").get(1).html();
+            String sign = getJsonValue("sign", js);
+            String action = getJsonValue("action", js);
+            String signs = getJsonValue("ajaxdata", js);
+            String webSignKey = getJsonValue("websignkey", js);
+            String webSign = "";
+
+            // OkHttp 获取直链
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("sign", sign)
+                    .add("action", action)
+                    .add("signs", signs)
+                    .add("websign", webSign)
+                    .add("websignkey", webSignKey)
+                    .add("ves", "1")
+                    .build();
+            Request request = new Request.Builder()
+                    .url("https://www.lanzoux.com/ajaxm.php")
+                    .post(formBody)
+                    .headers(headers)
+                    .build();
+            String dl;
+            try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                String body = response.body().string();
+                dl = getJsonValue("dom", body).replace("\\/", "/") + "/file/" + getJsonValue("url", body).replace("\\/", "/");
+            }
+
+            // 返回蓝奏云对象
+            return new Lanzou(name, size, dl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static String getJsonValue(String key, String json) {
+        return regex(json, "(?<=" + key + "['\"]?( )?[:=]( )?['\"]).+?(?=['\"])");
+    }
+
+    private static String getMainHost(String url) {
+        Pattern p = Pattern.compile("(?<=http://|\\.)[^.]*?\\.(?:com\\.cn|net\\.cn|org\\.cn|com|net|org|cn|biz|info|cc|tv)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = p.matcher(url);
+        if (matcher.find()) return (matcher.group());
+        return null;
+    }
+
+    private static String regex(String words, String regex) {
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = p.matcher(words);
+        if (matcher.find()) return (matcher.group());
+        return null;
+    }
+
+    private static String getFullHost(String url) {
+        Pattern p = Pattern.compile("[^//]*?\\.(com|cn|net|org|biz|info|cc|tv)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = p.matcher(url);
+        if (matcher.find()) return (matcher.group());
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> urls = new ArrayList<>();
+        //企业链接
+        urls.add("https://macwk.lanzouo.com/iRH0wwq0m8d");
+        //个人链接
+        urls.add("https://vincentapps.lanzouo.com/i4uS3lmp56j");
+        for (String url :
+                urls) {
+            System.out.println(getLanZouRealLink(url));
+        }
+    }
+
+    public static class Lanzou {
+        String name;
+        String size;
+        String dlLink;
+
+        public Lanzou(String name, String size, String dlLink) {
+            this.name = name;
+            this.size = size;
+            this.dlLink = dlLink;
+        }
+
+        @Override
+        public String toString() {
+            return "name='" + name + '\'' + '\n' +
+                    ", size='" + size + '\'' + '\n' +
+                    ", dlLink='" + dlLink + '\'' + '\n';
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSize() {
+            return size;
+        }
+
+        public String getDlLink() {
+            return dlLink;
+        }
+    }
+}
